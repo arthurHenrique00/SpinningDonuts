94faa8f77490a0424b98c20f08c1ed34c102be21
Arthur Henrique <arthurcostalima88@gmail.com>
2024-12-09 17:01:10 -0300

    desisto por enquanto

--- spinning_donuts.py
+++ spinning_donuts.py
@@ -6,18 +6,24 @@ import shutil
 def spinning_donut():
     A = 0
     B = 0
-    
-    while True:
 
+    while True:
+        
         cols, rows = shutil.get_terminal_size()
-        width = cols - 1
+        width = cols - 1 
         height = rows - 1
 
+        aspect_ratio = 2 
+        scaled_width = width // aspect_ratio
+
         output = [" "] * (width * height)
-        zbuffer = [0] * (width * height)
-        
-        for theta in range(0, 628, 7):
-            for phi in range(0, 628, 2):
+        zbuffer = [-float("inf")] * (width * height)
+
+        centerX = width // 2
+        centerY = height // 2
+
+        for theta in range(0, 628, 10):
+            for phi in range(0, 628, 3): 
 
                 sinA = math.sin(A)
                 cosA = math.cos(A)
@@ -27,29 +33,30 @@ def spinning_donut():
                 cosTheta = math.cos(theta / 100)
                 sinPhi = math.sin(phi / 100)
                 cosPhi = math.cos(phi / 100)
-                
+
                 circleX = cosTheta + 2
                 circleY = sinTheta
-                
+
                 x = circleX * (cosB * cosPhi + sinA * sinB * sinPhi) - circleY * cosA * sinB
                 y = circleX * (sinB * cosPhi - sinA * cosB * sinPhi) + circleY * cosA * cosB
                 z = 1 / (circleX * sinA * sinPhi + circleY * sinA * cosB + cosA * cosPhi + 5)
-                
-                screenX = int(width / 2 + (width / 4) * z * x)
-                screenY = int(height / 2 + (height / 4) * z * y)
-                
+
+                screenX = int(centerX + scaled_width * z * x)
+                screenY = int(centerY + height * z * y / 2)
+
                 brightness = int(8 * (cosTheta * cosPhi * sinB - cosA * cosTheta * sinPhi + sinTheta * cosB))
                 brightness = max(0, min(7, brightness))
-                
-                if 0 <= screenX < width and 0 <= screenY < height and z > zbuffer[screenY * width + screenX]:
-                    zbuffer[screenY * width + screenX] = z
-                    output[screenY * width + screenX] = " .:-=+*#@"[brightness]
-        
+
+                if 0 <= screenX < width and 0 <= screenY < height:
+                    idx = screenY * width + screenX
+                    if z > zbuffer[idx]:
+                        zbuffer[idx] = z
+                        output[idx] = " .:-=+*#@"[brightness]
+
         os.system("cls" if os.name == "nt" else "clear")
         print("\n".join("".join(output[i:i + width]) for i in range(0, len(output), width)))
-        A += 0.04
-        B += 0.08
+        A += 0.03
+        B += 0.07
         time.sleep(0.03)
 
 spinning_donut()
-
